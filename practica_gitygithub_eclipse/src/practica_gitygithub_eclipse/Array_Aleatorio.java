990af99da52dd3ce6ce9116dc387faa4b23cb770
MagcDam <a_miguelangel.gutierrez.calderon@iespablopicasso.es>
2023-01-26 13:59:49 +0100

    creamos el contador de veces que aparecen los numeros

--- practica_gitygithub_eclipse/src/practica_gitygithub_eclipse/Array_Aleatorio.java
+++ practica_gitygithub_eclipse/src/practica_gitygithub_eclipse/Array_Aleatorio.java
@@ -9,7 +9,13 @@ public class Array_Aleatorio {
     
     for (int i = 0; i < CANTIDADNUMEROS; i++) {
       numerosAleatorios[i] = (int) (Math.random()*(NUMEROMAXIMO + 1));
-      System.out.println(numerosAleatorios[i]);
+      int repetidos [] = new int [CANTIDADNUMEROS];
+      for (int j = 0; j < CANTIDADNUMEROS; j++) {
+        if (numerosAleatorios [i] == i) {
+          repetidos[j]++;
+        }
+        System.out.println(repetidos[j]);
+      }
     }
 
   }
