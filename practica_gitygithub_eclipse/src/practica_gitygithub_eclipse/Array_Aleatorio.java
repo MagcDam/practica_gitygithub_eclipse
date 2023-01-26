b388dda6e401ad36149b8d4a0fef4e8abaf8c73a
MagcDam <a_miguelangel.gutierrez.calderon@iespablopicasso.es>
2023-01-26 13:16:17 +0100

    añadimos la funcion print al programa

--- practica_gitygithub_eclipse/src/practica_gitygithub_eclipse/Array_Aleatorio.java
+++ practica_gitygithub_eclipse/src/practica_gitygithub_eclipse/Array_Aleatorio.java
@@ -9,8 +9,9 @@ public class Array_Aleatorio {
     
     for (int i = 0; i < CANTIDADNUMEROS; i++) {
       numerosAleatorios[i] = (int) (Math.random()*(NUMEROMAXIMO + 1));
+      System.out.println(numerosAleatorios[i]);
     }
-
+    
   }
 
 }
