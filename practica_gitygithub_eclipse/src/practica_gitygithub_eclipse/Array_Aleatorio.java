5ff3d8a4fc56f92e37a8ea63ece6c0eddef8c5ac
MagcDam <a_miguelangel.gutierrez.calderon@iespablopicasso.es>
2023-01-26 14:03:06 +0100

    ponemos la cantidad de numeros aleatorios a 30

--- practica_gitygithub_eclipse/src/practica_gitygithub_eclipse/Array_Aleatorio.java
+++ practica_gitygithub_eclipse/src/practica_gitygithub_eclipse/Array_Aleatorio.java
@@ -4,7 +4,7 @@ public class Array_Aleatorio {
 
   public static void main(String[] args) {
     final int NUMEROMAXIMO = 10;
-    final int CANTIDADNUMEROS = 20;
+    final int CANTIDADNUMEROS = 30;
     int[] numerosAleatorios = new int[CANTIDADNUMEROS];
     
     for (int i = 0; i < CANTIDADNUMEROS; i++) {
