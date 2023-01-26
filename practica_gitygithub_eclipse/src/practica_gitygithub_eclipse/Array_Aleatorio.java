0c6bef379d413115b05790c54cc7eccac67654ab
MagcDam <a_miguelangel.gutierrez.calderon@iespablopicasso.es>
2023-01-26 12:28:32 +0100

    archivos y primer programa

---
+++ practica_gitygithub_eclipse/src/practica_gitygithub_eclipse/Array_Aleatorio.java
@@ -0,0 +1,16 @@
+package practica_gitygithub_eclipse;
+
+public class Array_Aleatorio {
+
+  public static void main(String[] args) {
+    final int NUMEROMAXIMO = 10;
+    final int CANTIDADNUMEROS = 20;
+    int[] numerosAleatorios = new int[CANTIDADNUMEROS];
+    
+    for (int i = 0; i < CANTIDADNUMEROS; i++) {
+      numerosAleatorios[i] = (int) (Math.random()*(NUMEROMAXIMO + 1));
+    }
+
+  }
+
+}
